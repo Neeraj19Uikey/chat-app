ff484f2c9da44eb58056cf17517ef9d4c2ea6a55
Neeraj <uikeyneeraj2001@gmail.com>
2025-01-11 02:16:15 +0530

    added message function in the app

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import messageRoutes from "./routes/message.route.js";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 app.listen(PORT, () => {
   console.log("server is running on PORT: " + PORT);
   connectDB();
